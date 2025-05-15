071a33079332f88fb4481d0d02581911c349f519
kelikovic <kkinyama@zesco.co.zm>
2025-05-15 09:48:07 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -250,29 +250,40 @@ Route::middleware(['auth', 'role:super-admin|admin|agent'])->group(function () {
     Route::post('/change-password', [PasswordController::class, 'update']);
 
 
-    Route::get('/', DashboardIndex::class)->name('reports.index');
-    Route::get('/reports/index', DashboardIndex::class)->name('reports.index');
-    Route::get('/reports/call/summary/records', CallSummaryRecords::class)->name('reports.call.summary.records');
-    Route::get('/reports/call/detail/records', CallDetailRecords::class)->name('reports.call.detail.records');
-    Route::get('/reports/show/cdr/{id}', CallDetailRecords::class)->name('reports.show.cdr');
-    Route::get('/reports/search', SearchCDR::class)->name('reports.search');
 
-    // User Management
-    Route::get('/users/profile', UserProfile::class)->name('user.profile');
-    Route::get('/users/show/{id}', ShowUser::class)->name('user.show');
-    Route::get('/users/create-user', CreateNewUser::class)->name('user.create');
-    Route::get('/users/list-all', ListAllUsers::class)->name('user.list');
-    Route::get('/users/block/index', SuspendIndex::class)->name('suspend.index');
-    Route::get('/users/suspend/all/create', SuspendAllForm::class)->name('suspend.all.create');
-    Route::get('/users/suspend/{user}/create', SuspendForm::class)->name('suspend.one.create');
-    Route::get('/users/un-suspend/all/create', UnSuspendAllForm::class)->name('un-suspend.all.create');
-    Route::get('/users/un-suspend/{user}/create', UnSuspendForm::class)->name('un-suspend.one.create');
 
-    // Configuration
-    Route::get('/config/destinations', Destination::class)->name('config.destinations');
-    Route::get('/config/contexts', Contexts::class)->name('config.contexts');
-    Route::get('/config/show/contexts/{id}', ShowContexts::class)->name('config.show.contexts');
-    Route::get('/configurations/pbx-credentials', PbxCredentials::class)->name('configurations.pbx-credentials');
+    Route::middleware(['role:super-admin|admin'])->group(function () {
+        // Password Management
+        Route::get('/', DashboardIndex::class)->name('reports.index');
+        Route::get('/reports/index', DashboardIndex::class)->name('reports.index');
+        Route::get('/reports/call/summary/records', CallSummaryRecords::class)->name('reports.call.summary.records');
+        Route::get('/reports/call/detail/records', CallDetailRecords::class)->name('reports.call.detail.records');
+        Route::get('/reports/show/cdr/{id}', CallDetailRecords::class)->name('reports.show.cdr');
+        Route::get('/reports/search', SearchCDR::class)->name('reports.search');
+    });
+
+
+
+    Route::middleware(['role:super-admin|admin'])->group(function () {
+        // User Management
+        Route::get('/users/profile', UserProfile::class)->name('user.profile');
+        Route::get('/users/show/{id}', ShowUser::class)->name('user.show');
+        Route::get('/users/create-user', CreateNewUser::class)->name('user.create');
+        Route::get('/users/list-all', ListAllUsers::class)->name('user.list');
+        Route::get('/users/block/index', SuspendIndex::class)->name('suspend.index');
+        Route::get('/users/suspend/all/create', SuspendAllForm::class)->name('suspend.all.create');
+        Route::get('/users/suspend/{user}/create', SuspendForm::class)->name('suspend.one.create');
+        Route::get('/users/un-suspend/all/create', UnSuspendAllForm::class)->name('un-suspend.all.create');
+        Route::get('/users/un-suspend/{user}/create', UnSuspendForm::class)->name('un-suspend.one.create');
+    });
+
+    Route::middleware(['role:super-admin|admin'])->group(function () {
+        // Configuration
+        Route::get('/config/destinations', Destination::class)->name('config.destinations');
+        Route::get('/config/contexts', Contexts::class)->name('config.contexts');
+        Route::get('/config/show/contexts/{id}', ShowContexts::class)->name('config.show.contexts');
+        Route::get('/configurations/pbx-credentials', PbxCredentials::class)->name('configurations.pbx-credentials');
+    });
 
     // Live Components
     // Route::get('/live/dashboard', DashboardController::class)->name('live.dashboard');
