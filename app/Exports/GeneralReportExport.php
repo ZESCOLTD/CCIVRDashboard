24a32baac930ae2bcd3e9b313102167a1d0f0258
kelikovic <kkinyama@zesco.co.zm>
2025-07-17 14:26:31 +0200

    Update GeneralReportExport.php

--- app/Exports/GeneralReportExport.php
+++ app/Exports/GeneralReportExport.php
@@ -2,16 +2,43 @@
 
 namespace App\Exports;
 
-use App\Models\CDR\CallDetailsRecordModel;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromCollection; // or FromArray
+use Maatwebsite\Excel\Concerns\WithHeadings; // Often useful for headers
 
-class GeneralReportExport implements FromCollection
+class GeneralReportExport implements FromCollection, WithHeadings // or FromArray
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    protected $data;
+    protected $title;
+
+    public function __construct(array $data, string $title)
+    {
+        $this->data = $data;
+        $this->title = $title;
+    }
+
     public function collection()
     {
-        return CallDetailsRecordModel::all();
+        // If reportData is already an array of arrays, return it directly
+        return collect($this->data);
     }
+
+    public function headings(): array
+    {
+        // Define your column headers based on the keys in your reportData
+        // For 'Overall Queue Performance', this might look like:
+        if (!empty($this->data)) {
+            return array_keys($this->data[0]);
+        }
+        return [];
+    }
+    // You might also need a map() method if your data needs transformation
+    // public function map($row): array
+    // {
+    //     return [
+    //         $row['label'],
+    //         $row['total_calls'],
+    //         $row['answered'],
+    //         // ... and so on for all fields you want to export
+    //     ];
+    // }
 }
